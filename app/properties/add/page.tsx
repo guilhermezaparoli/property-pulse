aca68883bdc4245b468f5d3b61bf9c8f2a8f6a34
Guilherme <guilhermezapas@gmail.com>
2024-06-16 12:12:28 -0300

    Create of PropertyAddForm component with functional fields

--- app/properties/add/page.tsx
+++ app/properties/add/page.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import PropertyAddForm from '@/components/PropertyAddForm';
+import React from 'react';
 
 const PropertiesAddPage = () => {
   return (
-    <div>
-      PropertiesAddPage
-    </div>
-  )
-}
+    <section className="bg-blue-50">
+      <div className="container m-auto max-w-2xl py-24">
+        <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
+          <PropertyAddForm/>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default PropertiesAddPage
+export default PropertiesAddPage;
