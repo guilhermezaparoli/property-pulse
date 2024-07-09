55ce690387829d4432ff94fb2b269e0f721dbe1e
Guilherme <guilhermezapas@gmail.com>
2024-07-09 10:20:32 -0300

    creating of api PUT to update properties and edit page

--- app/api/properties/[id]/route.ts
+++ app/api/properties/[id]/route.ts
@@ -71,3 +71,67 @@ export const DELETE = async (
     });
   }
 };
+
+export const PUT = async (request: NextRequest, { params }) => {
+  try {
+    await connectDB();
+
+    const sessionUser = await getSessionUser();
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response('Unathorized', {
+        status: 401,
+      });
+    }
+    const { id } = params;
+    const { userId } = sessionUser;
+    const formData = await request.formData();
+    console.log(formData);
+    const amenities = formData.getAll('amenities');
+console.log(id)
+    const existingProperty = await Property.findById(id);
+    if (!existingProperty) {
+      return new Response('Property does not exist', { status: 404 });
+    }
+
+    if (existingProperty.owner.toString() !== userId) {
+      return new Response('Unauthorized', { status: 401 });
+    }
+    const propertyData = {
+      type: formData.get('type'),
+      name: formData.get('name'),
+      description: formData.get('description'),
+      location: {
+        street: formData.get('location.street'),
+        city: formData.get('location.city'),
+        state: formData.get('location.state'),
+        zipcode: formData.get('location.zipcode'),
+      },
+      beds: formData.get('beds'),
+      baths: formData.get('baths'),
+      square_feet: formData.get('square_feet'),
+      amenities,
+      rates: {
+        weekly: formData.get('rates.weekly'),
+        monthly: formData.get('rates.monthly'),
+        nightly: formData.get('rates.nightly'),
+      },
+      seller_info: {
+        name: formData.get('seller_info.name'),
+        email: formData.get('seller_info.email'),
+        phone: formData.get('seller_info.phone'),
+      },
+      owner: userId,
+    };
+
+    console.log(propertyData);
+
+   const updatedProperty = await Property.findByIdAndUpdate(id, propertyData);
+
+    return new Response(JSON.stringify(updatedProperty), {
+      status: 200,
+    });
+  } catch (error) {
+    console.error(error);
+    return new Response('Failed to add property', { status: 500 });
+  }
+};
