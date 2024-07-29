603d78ee75399d188524b469b9c38fe1662cc344
Guilherme <guilhermezapas@gmail.com>
2024-07-28 21:55:07 -0300

    adjust for deployment

--- components/HomeProperties.tsx
+++ components/HomeProperties.tsx
@@ -5,9 +5,9 @@ import { fetchProperties } from '@/utils/requests';
 
 const HomeProperties = async () => {
   const properties = await fetchProperties();
-  const recentProperties = properties
+  const recentProperties = properties.length > 0 ? properties
     ?.sort(() => Math.random() - Math.random())
-    .slice(0, 3);
+    .slice(0, 3) : [];
   return (
     <>
       <section className="px-4 py-6">
