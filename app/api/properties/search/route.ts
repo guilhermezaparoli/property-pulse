48edc0e4bf968805bf34860048ea3e241a3e0236
Guilherme <guilhermezapas@gmail.com>
2024-08-04 11:03:26 -0300

    Adding force-dynamic api

--- app/api/properties/search/route.ts
+++ app/api/properties/search/route.ts
@@ -1,7 +1,7 @@
 import connectDB from '@/config/database';
 import Property from '@/models/Property';
 import { NextRequest } from 'next/server';
-
+export const dynamic = 'force-dynamic';
 // GET /api/properties/search
 export const GET = async (request: NextRequest) => {
   try {
